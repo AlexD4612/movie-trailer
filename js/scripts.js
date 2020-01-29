23cb62fc6c9e7c63e44af4f014ed85e07a94f77b
AlexD4612 <59583079+AlexD4612@users.noreply.github.com>
2020-01-29 14:28:10 -0800

    Changed Controls

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,12 @@
 console.log('OK');
-const player = new Plyr('#player');
+const player = new Plyr('#player', {
+  // Turn on/off conrols. Array data type
+  controls : [ 'play', 'current-time', 'progress' ],
+  
+  // Turn on keyboard. Boolean data type
+  keyboard : { focused: true, global: true },
+  
+  // Increase/Decrease seek time. Integer data type
+  seekTime : 15
+
+});
