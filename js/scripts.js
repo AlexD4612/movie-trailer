bb34d51e6243a6cd5f975cb9e4d5818caa61d85b
AlexD4612 <59583079+AlexD4612@users.noreply.github.com>
2020-02-02 02:10:10 -0800

    Test
    Testing images for background, possible overlay in place, image needed still

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 console.log('OK');
 const player = new Plyr('#player', {
   // Turn on/off conrols. Array data type
-  controls : [ 'play', 'current-time', 'progress' ],
+  controls : [ 'play-large'],
   
   // Turn on keyboard. Boolean data type
   keyboard : { focused: true, global: true },
